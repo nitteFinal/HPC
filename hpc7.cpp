38d5ed1123a392d5f7becb865eabe9356be39dcc
nitteFinal <110346395+nitteFinal@users.noreply.github.com>
2023-02-09 12:37:58 +0530

    Update hpc7.cpp

--- hpc7.cpp
+++ hpc7.cpp
@@ -24,3 +24,56 @@ int main(int argc, char* argv[]) {
 	printf("process %d recieved %d\n", Pid, recv);
 	MPI_Finalize();
 }
+
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	int rank, size;
+	int data, gatheredData[100];
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	srand(rank);
+	data = rand()%10;
+	printf("Process %d sending data %d\n", rank, data);
+	
+	MPI_Gather(&data, 1, MPI_INT, gatheredData, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	if (rank == 0) {
+		printf("Process %d has gathered data: ", rank);
+		for (int i = 0; i < size; i++) {
+			printf("%d ", gatheredData[i]);
+		}
+		printf("\n");
+	}
+
+	MPI_Finalize();
+	return 0;
+}
+
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	int rank, size;
+	int data;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	if (rank == 0) {
+		data = 100;
+		printf("Process %d broadcasting data %d\n", rank, data);
+	}
+
+	MPI_Bcast(&data, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	printf("Process %d received data %d\n", rank, data);
+
+	MPI_Finalize();
+	return 0;
+}
